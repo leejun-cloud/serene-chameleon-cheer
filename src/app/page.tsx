0830dd8b8f38528308d0e6881b0b90138ce846b9
[dyad] <duoenjia8@gmail.com>
2025-07-26 17:19:28 +0900

    [dyad] 홈 페이지에 뉴스레터 생성기 표시 - wrote 1 file(s) + extra files edited outside of Dyad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+"use client"; // NewsletterCreatorPage가 클라이언트 컴포넌트이므로 이 파일도 클라이언트 컴포넌트로 만듭니다.
+
+import NewsletterCreatorPage from "@/app/newsletter/page";
 import { MadeWithDyad } from "@/components/made-with-dyad";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
-        <h1>Blank page</h1>
-      </main>
+    <>
+      <NewsletterCreatorPage />
       <MadeWithDyad />
-    </div>
+    </>
   );
 }
